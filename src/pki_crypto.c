402c922a9e29515e2546231c091560e360ff2fc7
Andreas Schneider <asn@cryptomilk.org>
2020-01-23 09:15:29 +0100

    pki_crypto: Fix possible memory leak on error
    CID #1409680
    
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>
    (cherry picked from commit 72498bac5fcaf92b11ccb58798ad8c6c0fcbee71)

--- src/pki_crypto.c
+++ src/pki_crypto.c
@@ -1569,9 +1569,9 @@ static int pki_signature_from_rsa_blob(const ssh_key pubkey,
                                        ssh_signature sig)
 {
     uint32_t pad_len = 0;
-    char *blob_orig;
-    char *blob_padded_data;
-    ssh_string sig_blob_padded;
+    char *blob_orig = NULL;
+    char *blob_padded_data = NULL;
+    ssh_string sig_blob_padded = NULL;
 
     size_t rsalen = 0;
     size_t len = ssh_string_len(sig_blob);
@@ -1629,6 +1629,7 @@ static int pki_signature_from_rsa_blob(const ssh_key pubkey,
     return SSH_OK;
 
 errout:
+    SSH_STRING_FREE(sig_blob_padded);
     return SSH_ERROR;
 }
 
