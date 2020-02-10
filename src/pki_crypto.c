0dd71375e7fe3ba80a225ffb4304319a756aee78
Jon Simons <jon@jonsimons.org>
2020-02-10 13:37:14 -0500

    pki: fix `pki_key_ecdsa_to_key_type` thread-safety
    Resolves https://bugs.libssh.org/T214.
    
    Signed-off-by: Jon Simons <jon@jonsimons.org>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>
    (cherry picked from commit 3664ba2800eaa1b54008b232d7f0404871d2f8d9)

--- src/pki_crypto.c
+++ src/pki_crypto.c
@@ -94,7 +94,7 @@ static int pki_key_ecdsa_to_nid(EC_KEY *k)
 
 static enum ssh_keytypes_e pki_key_ecdsa_to_key_type(EC_KEY *k)
 {
-    static int nid;
+    int nid;
 
     nid = pki_key_ecdsa_to_nid(k);
 
