deb9fc015ebdb3b4844c67a1285524a75c258d88
Jakub Jelen <jjelen@redhat.com>
2020-10-02 11:24:58 +0200

    torture: Place additional configuration options before defaults so they can override them
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
    (cherry picked from commit f9bd1db8c3a936edd11fcfa97999d19fc1020600)

--- tests/torture.c
+++ tests/torture.c
@@ -629,7 +629,8 @@ static void torture_setup_create_sshd_config(void **state, bool pam)
              "\n"
              "StrictModes no\n"
              "\n"
-             "%s" /* Here comes UsePam */
+             "%s\n" /* Here comes UsePam */
+             "%s" /* The space for test-specific options */
              "\n"
              /* add all supported algorithms */
              "HostKeyAlgorithms " OPENSSH_KEYS "\n"
@@ -644,8 +645,7 @@ static void torture_setup_create_sshd_config(void **state, bool pam)
              "AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT\n"
              "AcceptEnv LC_IDENTIFICATION LC_ALL LC_LIBSSH\n"
              "\n"
-             "PidFile %s\n"
-             "%s\n"; /* The space for test-specific options */
+             "PidFile %s\n";
     /* FIPS config */
     const char fips_config_string[]=
              "Port 22\n"
@@ -663,7 +663,8 @@ static void torture_setup_create_sshd_config(void **state, bool pam)
              "\n"
              "StrictModes no\n"
              "\n"
-             "%s" /* UsePam */
+             "%s\n" /* Here comes UsePam */
+             "%s" /* The space for test-specific options */
              "\n"
              "Ciphers "
                 "aes256-gcm@openssh.com,aes256-ctr,aes256-cbc,"
@@ -692,8 +693,7 @@ static void torture_setup_create_sshd_config(void **state, bool pam)
              "AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT\n"
              "AcceptEnv LC_IDENTIFICATION LC_ALL LC_LIBSSH\n"
              "\n"
-             "PidFile %s\n" /* PID file */
-             "%s\n"; /* The space for test-specific options */
+             "PidFile %s\n"; /* PID file */
     const char usepam_yes[] =
              "UsePAM yes\n"
              "KbdInteractiveAuthentication yes\n";
@@ -794,8 +794,8 @@ static void torture_setup_create_sshd_config(void **state, bool pam)
                 trusted_ca_pubkey,
                 sftp_server,
                 usepam,
-                s->srv_pidfile,
-                additional_config);
+                additional_config,
+                s->srv_pidfile);
     } else {
         snprintf(sshd_config, sizeof(sshd_config),
                 config_string,
@@ -808,8 +808,8 @@ static void torture_setup_create_sshd_config(void **state, bool pam)
                 trusted_ca_pubkey,
                 sftp_server,
                 usepam,
-                s->srv_pidfile,
-                additional_config);
+                additional_config,
+                s->srv_pidfile);
     }
 
     torture_write_file(s->srv_config, sshd_config);
