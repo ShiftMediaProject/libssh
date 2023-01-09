edcce095e0ddb0701e7532d1c61eb4aeff5dabc6
Andreas Schneider <asn@cryptomilk.org>
2023-01-09 09:06:38 +0100

    Happy new year 2023!
    And happy anniversary libssh (20 years).
    
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>
    Reviewed-by: Jakub Jelen <jjelen@redhat.com>

--- src/client.c
+++ src/client.c
@@ -842,7 +842,7 @@ error:
  */
 const char *ssh_copyright(void)
 {
-    return SSH_STRINGIFY(LIBSSH_VERSION) " (c) 2003-2022 "
+    return SSH_STRINGIFY(LIBSSH_VERSION) " (c) 2003-2023 "
            "Aris Adamantiadis, Andreas Schneider "
            "and libssh contributors. "
            "Distributed under the LGPL, please refer to COPYING "
