8d0d3d4d7baaf73d32ba1dd85c16b94b467e1ab6
Jakub Jelen <jjelen@redhat.com>
2024-08-16 16:12:58 +0200

    Add explicit -Werror=unused-variable
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>
    Reviewed-by: Eshan Kelkar <eshankelkar@galorithm.com>
    (cherry picked from commit 8295945011025305c97de879173fb9f239d4bb79)

--- CompilerChecks.cmake
+++ CompilerChecks.cmake
@@ -48,6 +48,7 @@ if (UNIX)
     add_c_compiler_flag("-Werror=implicit-int" SUPPORTED_COMPILER_FLAGS)
     add_c_compiler_flag("-Wint-conversion" SUPPORTED_COMPILER_FLAGS)
     add_c_compiler_flag("-Werror=int-conversion" SUPPORTED_COMPILER_FLAGS)
+    add_c_compiler_flag("-Werror=unused-variable" SUPPORTED_COMPILER_FLAGS)
 
     check_c_compiler_flag("-Wformat" REQUIRED_FLAGS_WFORMAT)
     if (REQUIRED_FLAGS_WFORMAT)
